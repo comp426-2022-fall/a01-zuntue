1dfd4c2321cdcaeb8fecba1aa47b5e1d79eb9031
zuntue <zuntue@gmail.com>
2022-12-08 19:32:34 -0500

    not getting any errors

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 // Require http module
-
+const http = require('http')
 // Require fs module
-
+const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
+const minimist = require('minimist')
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
+const arguments = process.argv.slice(2);
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = arguments['port'] || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
 
-// If there is an error, put it on the console error and return. 
+// If there is an error, put it on the console error and return.
 // Do not be nice about exiting.
-
-
-
-
+fs.readFile(`./public/index.html`, 'utf8', (error, data) => {
+    if (error) {
+        console.error(error);
+    }
+})
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -28,14 +31,17 @@
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
-
+const server = http.createServer((request, response) => {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html')
+    response.end(data)
+})
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
-
-
+// Put the exact message `Server listening on port ${port}` on the console log.
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
 
 // That's it! You're all done!
