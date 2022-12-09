3d77d8aa16c0a625562148a8db3005d049898aca
zuntue <zuntue@gmail.com>
2022-12-08 19:39:40 -0500

    changed how arguments are handled

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const http = require('http')
 // Require fs module
 const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
-const minimist = require('minimist')
+const minimist = require('minimist')(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const arguments = process.argv.slice(2);
+// const arguments = process.argv.slice(2);
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
